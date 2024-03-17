98b11840068cf96b77b029788e0a317102f61dc9
srikarpeesapati22 <112534138+srikarpeesapati22@users.noreply.github.com>
2024-03-17 22:47:57 +0000

    Modified header and footer to be dynamically included

---
+++ scripts/header.js
@@ -0,0 +1,100 @@
+//Header
+document.getElementById('header-placeholder').innerHTML = `
+<!--Header-->
+<header class="header" id="header">
+    <div class="wrap">
+        <div class="inner">
+            <!--Header Logo-->
+            <div class="header_left">
+                <img src="../images/logointergrated.png" alt="" class="header_logo">
+            </div>
+
+            <!--Header Links-->
+            <div class="header_right">
+                <a href="./../html/index.html" class="header_link">
+                    <p class="before_open_menu">Home</p>
+                    <div class="header_underbox">
+                        <p>ABSTRACT</p>
+                        <p>FEATURES</p>
+                        <p>VIDEO</p>
+                        <p>OUR TEAM</p>
+                        <p>PROJECT TIMELINE</p>
+                    </div>
+                </a>
+
+                <a href="./../html/Requirements.html" class="header_link">
+                    <p class="before_open_menu">Requirements</p>
+                    <div class="header_underbox">
+                        <p>PROJECT BACKGROUND</p>
+                        <p>CLIENT REQUIREMENTS</p>
+                        <p>PROJECT GOAL</p>
+                        <p>PERSONAS AND USE CASES</p>
+                        <p>USER INTERVIEW</p>
+                        <p>MOSCOW LIST</p>
+                    </div>
+                </a>
+                <a href="./../html/HCI.html" class="header_link">
+                    <p class="before_open_menu">HCI</p>
+                    <div class="header_underbox">
+                        <p>SKETCHES</p>
+                        <p>PROTOTYPE</p>
+                        <p>EVALUATION</p>
+                    </div>
+                </a>
+                <a href="./../html/Research.html" class="header_link">
+                    <p class="before_open_menu">Research</p>
+                    <div class="header_underbox">
+                        <p>PROJECT RESEARCH</p>
+                        <p>TECHNOLOGY RESEARCH</p>
+                        <p>TECHNICAL DECISIONS</p>
+                        <p>REFERENCES</p>
+                    </div>
+                </a>
+                <a href="./../html/SystemDesign.html" class="header_link">
+                    <p class="before_open_menu">System Design</p>
+                    <div class="header_underbox">
+                        <p>SYSTEM ARCHITECTURE</p>
+                        <p>SEQUENCE DIAGRAM</p>
+                        <p>FLOW DIAGRAM</p>
+                    </div>
+                </a>
+                <a href="./../html/Implementation.html" class="header_link">
+                    <p class="before_open_menu">Implementation</p>
+                    <div class="header_underbox">
+                        <p>IMPLEMENTATION 1</p>
+                        <p>IMPLEMENTATION 2</p>
+                    </div>
+                </a>
+                <a href="./../html/Testing.html" class="header_link">
+                    <p class="before_open_menu">Testing</p>
+                    <div class="header_underbox">
+                        <p>TESTING STRATEGY</p>
+                        <p>UNIT & INTEGRATION TESTING</p>
+                        <p>PERFORMANCE TESTING</p>
+                        <p>USER ACCEPTANCE TESTING</p>
+                        <p>CONCLUSION</p>
+                    </div>
+                </a>
+                <a href="./../html/Evaluation.html" class="header_link">
+                    <p class="before_open_menu">Evaluation</p>
+                    <div class="header_underbox">
+                        <p>MOSCOW EVALUATION</p>
+                        <p>BUG LIST</p>
+                        <p>CRITICAL EVALUATION</p>
+                        <p>INDIVIDUAL CONTRIBUTION</p>
+                        <p>FUTURE WORK</p>
+                    </div>
+                </a>
+                <a href="./../html/Appendix.html" class="header_link">
+                    <p class="before_open_menu">Appendix</p>
+                    <div class="header_underbox">
+                        <p>LINKS</p>
+                        <p>Monthly Videos</p>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+</header>
+<!--Header End-->
+`;
