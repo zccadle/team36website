7c94b488ef4323b9f3e331e4ff277dfcac8702bc
srikarpeesapati22 <112534138+srikarpeesapati22@users.noreply.github.com>
2024-03-20 17:41:09 +0000

    Home Page Changes

--- js/footer.js
+++ js/footer.js
@@ -1,8 +1,8 @@
 // Footer
 document.getElementById('footer-placeholder').innerHTML = `
 <!--Footer-->
-  <footer class="row row-cols-1 row-cols-sm-2 row-cols-md-5 py-5 my-5 border-top">
-
+  <footer class="row row-cols-1 row-cols-sm-2 row-cols-md-5 py-5 my-5 border-top justify-content-evenly bg-dark-subtle">
+  
     <div class="col mb-3">
       <h5>Data Visualization on IFRC GO</h5>
       <ul class="nav flex-column">
@@ -14,21 +14,21 @@ document.getElementById('footer-placeholder').innerHTML = `
     <div class="col mb-3">
       <h5>Site Navigation</h5>
       <ul class="nav flex-column">
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Requirements</a></li>
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Research</a></li>
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Testing</a></li>
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">UI Design</a></li>
+        <li class="nav-item mb-2"><a href="./../html/index.html" class="nav-link p-0 text-muted">Home</a></li>
+        <li class="nav-item mb-2"><a href="./../html/requirements.html" class="nav-link p-0 text-muted">Requirements</a></li>
+        <li class="nav-item mb-2"><a href="./../html/research.html" class="nav-link p-0 text-muted">Research</a></li>
+        <li class="nav-item mb-2"><a href="./../html/ui_design.html" class="nav-link p-0 text-muted">UI Design</a></li>
+        <li class="nav-item mb-2"><a href="./../html/systemdesign.html" class="nav-link p-0 text-muted">System Design</a></li>
       </ul>
     </div>
 
     <div class="col mb-3">
       <h5>     </h5>
       <ul class="nav flex-column">
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">System Design</a></li>
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Evaluation</a></li>
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">HCI</a></li>
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Appendix</a></li>
+        <li class="nav-item mb-2"><a href="./../html/implementation.html" class="nav-link p-0 text-muted">Implementation</a></li>
+        <li class="nav-item mb-2"><a href="./../html/testing.html" class="nav-link p-0 text-muted">Testing</a></li>
+        <li class="nav-item mb-2"><a href="./../html/evaluation.html" class="nav-link p-0 text-muted">Evaluation</a></li>
+        <li class="nav-item mb-2"><a href="./../html/appendix.html" class="nav-link p-0 text-muted">Appendix</a></li>
       </ul>
     </div>
     
@@ -36,7 +36,7 @@ document.getElementById('footer-placeholder').innerHTML = `
       <h5>Partners</h5>
       <ul class="nav flex-column">
         <li class="nav-item mb-2">
-            <a href="#" class="nav-link p-0 text-muted">
+            <a href="https://go.ifrc.org/" class="nav-link p-0 text-muted">
                 <img src="./../images/logointergrated.png" alt="Logo" class="img-fluid">
             </a>
         </li>
--- js/header.js
+++ js/header.js
@@ -1,44 +1,47 @@
 // Header
 document.getElementById('header-placeholder').innerHTML = `
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"/>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+<link href="./../styles.css" rel="stylesheet">
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="https://go.ifrc.org/">
-        <img src="./../images/logointergrated.png" alt="Logo" height = "100">
-    </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="./../html/index.html">Home</a>
-        </li>
-        <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="./../html/Requirements.html" id="navbarDropdownMenuLink" role="button" aria-expanded="false">
-                Requirements
-            </a>
-            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                <li><a class="dropdown-item" href="./../html/Requirements.html#ProjectBackground">Project Background</a></li>
-                <li><a class="dropdown-item" href="./../html/Requirements.html#ClientRequirements">Client Requirements</a></li>
-                <li><a class="dropdown-item" href="./../html/Requirements.html#ProjectGoal">Project Goal</a></li>
-                <li><a class="dropdown-item" href="./../html/Requirements.html#PersonaAndUseCases">Persona and Use Cases</a></li>
-                <li><a class="dropdown-item" href="./../html/Requirements.html#UserInterview">User Interview</a></li>
-                <li><a class="dropdown-item" href="./../html/Requirements.html#MOSCOWList">MOSCOW List</a></li>
+    <div class="container-fluid">
+        <a class="navbar-brand" href="https://go.ifrc.org/">
+            <img src="./../images/logointergrated.png" alt="Logo" height="100">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link ${window.location.pathname.includes('index.html') ? 'active' : ''}" href="./../html/index.html">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link ${window.location.pathname.includes('requirements.html') ? 'active' : ''}" href="./../html/requirements.html">Requirements</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link ${window.location.pathname.includes('research.html') ? 'active' : ''}" href="./../html/research.html">Research</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link ${window.location.pathname.includes('ui_design.html') ? 'active' : ''}" href="./../html/ui_design.html">UI Design</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link ${window.location.pathname.includes('systemdesign.html') ? 'active' : ''}" href="./../html/systemdesign.html">System Design</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link ${window.location.pathname.includes('implementation.html') ? 'active' : ''}" href="./../html/implementation.html">Implementation</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link ${window.location.pathname.includes('testing.html') ? 'active' : ''}" href="./../html/testing.html">Testing</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link ${window.location.pathname.includes('evaluation.html') ? 'active' : ''}" href="./../html/evaluation.html">Evaluation</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link ${window.location.pathname.includes('appendix.html') ? 'active' : ''}" href="./../html/appendix.html">Appendix</a>
+                </li>
             </ul>
-        </li>
-
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown link
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-      </ul>
+        </div>
     </div>
-  </div>
 </nav>
 `;
